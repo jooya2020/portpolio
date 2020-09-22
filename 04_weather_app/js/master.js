36f05790ada32de6a0224a31b80835640d697bc7
HwangOonha <dnsgk4983@gmail.com>
2020-09-22 11:32:48 +0900

    a

---
+++ 04_weather_app/js/master.js
@@ -0,0 +1,9 @@
+//swiper
+var swiper1 = new Swiper('.sub_weather_info',{
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true
+    }
+});
---
+++ 05_Landing_Page/js/js.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.hover').mouseover(function(){
+        $('.video_hover').fadeIn();
+    });
+    $('.hover').mouseleave(function(){
+        $('.video_hover').fadeout();
+    });
+})
